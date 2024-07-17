1599371f0018c2416da99d3ec96a0bda057a1bfb
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-07-17 11:44:30 -0300

    Update functions.js
    Add functions para calcular o peso ideal e mudança de cores na interação

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,41 @@
 $(function(){
 	$('#start').click(function(){
         var altura = $('#altura').val();
-        var peso = $('#kilos').val();
+        var peso = $('#kilos').val();        
         if(validarValores(altura) && validarValores(peso)){    
             var imc = (peso)/(altura*altura);
+            var pesoIdeal1 = 18.5*(altura*altura); 
+            var pesoIdeal2 = 24.9*(altura*altura);
             imc = imc.toFixed(1);
+            pesoIdeal1 = pesoIdeal1.toFixed(1); 
+            pesoIdeal2 = pesoIdeal2.toFixed(1);
             if(imc < 18.5){
-                $('#parte-oculta').css('color','darkred'); //abaixo do peso
+                $('#parte-oculta').css('color','red'); //abaixo do peso
                 $('#resultado').html(`IMC: ${imc}`);
+                $('#descricao').html('Você está abaixo do Peso. <br/> Peso ideal está entre: '+pesoIdeal1+' - '+pesoIdeal2);
+                $('#descricao').css('width','370px').css('height','100%');
             }else if(imc >= 18.5 && imc <= 24.9){
                 $('#parte-oculta').css('color','green'); 
                 $('#resultado').html(`IMC: ${imc}`); //peso saudável 
+                $('#descricao').html('Você está com peso SAUDÁVEL');
+                $('#descricao').css('width','370px').css('height','100%');
             }else if(imc >= 25.0 && imc <= 30.0){
-                $('#parte-oculta').css('color','darkred');
+                $('#parte-oculta').css('color','red');
                 $('#resultado').html(`IMC: ${imc}`);
+                $('#descricao').html('Você está ACIMA do peso. <br/> Peso ideal está entre: '+pesoIdeal1+' - '+pesoIdeal2);
+                $('#descricao').css('width','370px').css('height','100%');
                 //sobrepeso
             }else if(imc >= 30.1 && imc <= 39.9){
-                $('#parte-oculta').css('color','darkred');
+                $('#parte-oculta').css('color','red');
                 $('#resultado').html(`IMC: ${imc}`);
+                $('#descricao').html('Você está ACIMA do peso. <br/> Peso ideal está entre: '+pesoIdeal1+' - '+pesoIdeal2);
+                $('#descricao').css('width','370px').css('height','100%');
                 //obeso
             }else if(imc >= 40){
-                $('#parte-oculta').css('color','darkred');
+                $('#parte-oculta').css('color','red');
                 $('#resultado').html(`IMC: ${imc}`);
+                $('#descricao').html('Você está ACIMA do peso. <br/> Peso ideal está entre: '+pesoIdeal1+' - '+pesoIdeal2);
+                $('#descricao').css('width','370px').css('height','100%');
                 //obeso mórbido; 
             }
         }else{
