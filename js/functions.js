4ec15ef57e90dc789787a9f3514fbe26ad6cbc41
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-07-16 22:33:37 -0300

    Add CalcIMC
    Add o calculo do IMC e captura de valores

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
 $(function(){
-	alert("Olá mundo");
+	$('#start').click(function(){
+        var altura = $('#altura').val();
+        var peso = $('#kilos').val();
+        var imc = (peso)/(altura*altura);
+        console.log(imc);
+    });
 })
